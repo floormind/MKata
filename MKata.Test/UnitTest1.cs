83f877c7e84963b7cdbad84019d487a26b982809
Ife <ife_labolz@hotmail.com>
2020-11-04 23:39:22 +0000

    Breaking down the requirements

--- MKata.Test/UnitTest1.cs
+++ MKata.Test/UnitTest1.cs
@@ -2,6 +2,18 @@ using NUnit.Framework;
 
 namespace MKata.Test
 {
+    // What
+    //    -> Ability to Scan PRODUCTS
+    //    -> Ability to ask for Total Price of Scanned items (-> Apply discount mechanism)
+    //    -> Create checkout (-> Apply discount mechanism)
+    // How
+    //    -> A way to hold the current list of products
+    //    -> A way to hold the special offers of the products
+    //    -> A Way to add scan product/products in to basket
+        
+    // Testing
+    //    -> Create Product object
+    //    -> Create a mechanism of applying the type of discount we want to apply
     public class Tests
     {
         [SetUp]
